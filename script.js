26015cae4fdf7bf0d39e4f5eedabc67ae3bd0432
Lucia Vu <luciavu6@outlook.com>
2024-05-08 18:31:50 +1000

    Retrieve move and score data

---
+++ script.js
@@ -0,0 +1,16 @@
+// Move icons
+var playerMove = document.querySelector("#playerMove");
+var computerMove = document.querySelector("#computerMove");
+
+// Scoreboard
+const playerScoreboard = document.querySelector(".playerScore");
+var playerScore = playerScoreboard.querySelector(".score");
+const computerScoreboard = document.querySelector(".computerScore");
+var computerScore = computerScoreboard.querySelector(".score");
+
+const MOVE_ICONS = ["icon-hand-grab-o", "icon-hand-paper-o", "icon-hand-scissors-o"];
+
+
+//playerScore.textContent = "1";
+//computerScore.textContent = "2";
+//playerMove.className = MOVE_ICONS[1];
